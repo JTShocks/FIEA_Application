19c170d0c68e29eab0d64ffd046d824985efe78b
JTShocks <jacobdreyer2002@gmail.com>
2025-01-31 13:03:42 -0500

    Creating functions for projectile and weapons controller

--- Assets/IProjectile.cs
+++ Assets/IProjectile.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public interface IProjectile
 {
+    float speed {get;set;}
     void Shoot(Vector3 position, Vector3 direction, float speed);
     void OnCollisionEnter(Collision other);
     void OnParticleSystemStopped();
--- Assets/WeaponController.cs
+++ Assets/WeaponController.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Transform shootLocation;
+    void OnFire()
     {
-        
+        GameObject bolt = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        SpawnBolt(bolt);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnBolt(GameObject instance)
     {
+        //Spawn the prefab and check if it is a projectile
+        IProjectile projectile = instance.GetComponent<IProjectile>();
+        if(projectile == null)
+        {
+            return;
+        }
         
+        Vector3 spawnLocation = new Vector3
+        (
+            shootLocation.transform.position.x,
+            shootLocation.transform.position.y,
+            shootLocation.transform.position.z
+        );
+
+        instance.transform.position = spawnLocation;
+
+        projectile.Shoot(spawnLocation, shootLocation.transform.forward, projectile.speed);
     }
 }
