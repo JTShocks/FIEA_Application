bbb2c12ee2ce9bfa0cab84a465423d9eedc6f81c
JTShocks <jacobdreyer2002@gmail.com>
2025-01-31 12:04:42 -0500

    Implementing code for player jumping

--- Assets/PlayerJumping.cs
+++ Assets/PlayerJumping.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerJumping : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float jumpPressBufferTime = .1f;
+    PlayerMovement player;
+    bool isTryingToJump;
+    float lastJumpPressTime;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        player = GetComponent<PlayerMovement>();
+    }
+    void OnEnable(){ player.OnBeforeMove += OnBeforeMove;}
+
+    void OnDisable(){player.OnBeforeMove -= OnBeforeMove;}
+
+    private void OnBeforeMove()
+    {
+        throw new NotImplementedException();
     }
 }
